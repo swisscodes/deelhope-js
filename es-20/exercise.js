7dc30035ab1329ec52684c93f1abac3eb1569b3a
Obafemi Lawal <swissbobo@gmail.com>
2022-09-19 23:43:54 +0300

    ex20

---
+++ es-20/exercise.js
@@ -0,0 +1,10 @@
+var primitive = true;
+
+if (
+  typeof primitive == 'boolean' ||
+  typeof primitive == 'string' ||
+  typeof primitive == 'number'
+) {
+  console.log(typeof primitive, primitive);
+} else {
+}
