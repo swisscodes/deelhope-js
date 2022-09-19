f841bbcf993c0c42145d83bbd70f0e3c41c79e88
Obafemi Lawal <swissbobo@gmail.com>
2022-09-19 23:15:37 +0300

    ex16

---
+++ es-16/exercise.js
@@ -0,0 +1,9 @@
+var isSunnyDay = true;
+var isHomeworkCompleted = true;
+console.log(
+  `${
+    isSunnyDay && isHomeworkCompleted
+      ? 'Jesse can go out to play'
+      : 'Jesse stays home'
+  }`
+);
