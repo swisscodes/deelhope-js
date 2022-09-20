e963df1e485a2e4f6287ee843bc0d0cca2decbea
Obafemi Lawal <swissbobo@gmail.com>
2022-09-20 12:52:06 +0300

    ex39

---
+++ es-39/exercise.js
@@ -0,0 +1,9 @@
+function multiplyByTwo(value) {
+  let number = 2;
+  if (value && typeof value === 'number') {
+    return () => console.log(value * number);
+  }
+  return () => console.log('please pass a number');
+}
+
+multiplyByTwo('t')();
