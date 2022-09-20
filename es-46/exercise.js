e668dabc5c3258df3bdaae79a7fb0861806971ea
Obafemi Lawal <swissbobo@gmail.com>
2022-09-20 14:07:28 +0300

    ex46

---
+++ es-46/exercise.js
@@ -0,0 +1,12 @@
+const student = {
+  id: 1,
+  school: 'Liceo',
+  year: 3,
+  personalData: {
+    name: 'Obafemi',
+    surname: 'Lawal',
+    age: 35,
+  },
+};
+
+console.log(student.personalData);
