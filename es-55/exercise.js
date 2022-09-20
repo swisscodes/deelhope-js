d43fc1dc6e3408f26577c5b8f23032a35816c5d6
Obafemi Lawal <swissbobo@gmail.com>
2022-09-20 18:42:35 +0300

    ex55

---
+++ es-55/exercise.js
@@ -0,0 +1,36 @@
+function calculateAverageAge(persons) {
+  // ...
+}
+
+const persons = [
+  { name: 'Paul', age: 16 },
+  { name: 'George', age: 17 },
+  { name: 'Lucas', age: 21 },
+  { name: 'Marco', age: 32 },
+  { name: 'Peter', age: 18 },
+  { name: 'Carl', age: 13 },
+  { name: 'Simon', age: 24 },
+  { name: 'Mark', age: 15 },
+  { name: 'Sandra', age: 34 },
+  { name: 'Alice', age: 28 },
+];
+
+const average = calculateAverageAge(persons);
+console.log(persons);
+console.log(average);
+
+function calculateAverageAge(people) {
+  var sum = 0;
+  for (let i = 0; i < people.length; i++) {
+    sum += people[i].age;
+  }
+  return sum;
+}
+
+//I Prefare this.
+
+// function calculateAverageAge(people) {
+//   return people.reduce((acc, person) => {
+//     return acc + person.age;
+//   }, 0);
+// }
