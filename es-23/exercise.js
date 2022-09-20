fc10edf29c5ac17b7adce05e9789da993295f18c
Obafemi Lawal <swissbobo@gmail.com>
2022-09-20 08:52:35 +0300

    ex23

---
+++ es-23/exercise.js
@@ -0,0 +1,19 @@
+var room = 'Master Suite';
+
+switch (room) {
+  case 'Basic Room': {
+    console.log('50€');
+    break;
+  }
+  case 'Junior Suite Room': {
+    console.log('80€');
+    break;
+  }
+  case 'Master Suite': {
+    console.log('100€');
+    break;
+  }
+
+  default:
+    break;
+}
