45197bcb87e51564e34d5c7db74503a97cbbd0f7
Obafemi Lawal <swissbobo@gmail.com>
2022-09-20 14:13:55 +0300

    ex47

---
+++ es-47/exercise.js
@@ -0,0 +1,8 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log(numbers[0]);
+console.log(numbers[numbers.length - 1]);
+console.log(numbers.length);
+
+//console.log(numbers[20])
+//out of range error we have used index that is out of range of the array length
