0d6a5a83490c26d161e99d04e569919140e460dd
Obafemi Lawal <swissbobo@gmail.com>
2022-09-20 14:34:26 +0300

    ex49

---
+++ es-49/exercise.js
@@ -0,0 +1,9 @@
+const students = [
+  { id: 1, name: 'Luca', surname: 'Rossi', age: 20 },
+  { id: 2, name: 'Mario', surname: 'Bianchi', age: 24 },
+  { id: 3, name: 'Giovanni', surname: 'Brambilla', age: 30 },
+];
+
+for (let index = 0; index < students.length; index++) {
+  console.log(students[index]);
+}
