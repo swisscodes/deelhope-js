0b35f097d14ed7aa18213afd982b63fff8d7e123
Obafemi Lawal <swissbobo@gmail.com>
2022-09-20 14:31:17 +0300

    ex48

---
+++ es-48/exercise.js
@@ -0,0 +1,7 @@
+const students = [
+  { id: 1, name: 'Obafemi', surname: 'Lawal', age: 35 },
+  { id: 2, name: 'Devel', surname: 'Hope', age: 3 },
+  { id: 3, name: 'Dave', surname: 'Squirrel', age: 55 },
+];
+
+console.log(students[0]);
