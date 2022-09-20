0f400cb47fe7ab1b718b80dfccb5d37bdc695452
Obafemi Lawal <swissbobo@gmail.com>
2022-09-20 09:49:51 +0300

    ex24

---
+++ es-24/exercise.js
@@ -0,0 +1,18 @@
+let num = 1;
+
+while (num <= 10) {
+  console.log(num);
+  num++;
+}
+
+// function numPrint(number) {
+//   if (number > 10) {
+//     console.log('done');
+//     return;
+//   }
+//   console.log(number);
+//   numPrint((number += 1));
+// }
+
+/* numPrint(1);  // just testing to see if i still understand recursion 
+ for my personal reference */
